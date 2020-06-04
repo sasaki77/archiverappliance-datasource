e7da4ded0a0b23415d43ec8f84e7a6873e8bf9e6
Shinya Sasaki <shinya.sasaki@kek.jp>
2020-06-05 08:59:52 +0900

    Add light theme mode

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,13 @@
-import { DataSource } from './DataSource';
 import { DataSourcePlugin } from '@grafana/data';
+import { loadPluginCss } from '@grafana/runtime';
+import { DataSource } from './DataSource';
 import { ConfigEditor, QueryEditor } from './components';
 import { AAQuery, AADataSourceOptions } from './types';
-import './css/query_editor.css';
+
+loadPluginCss({
+  dark: 'plugins/sasaki77-archiverappliance-datasource/styles/dark.css',
+  light: 'plugins/sasaki77-archiverappliance-datasource/styles/light.css',
+});
 
 export const plugin = new DataSourcePlugin<DataSource, AAQuery, AADataSourceOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
