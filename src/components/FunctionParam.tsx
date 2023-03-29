fc3e8b89cef07799d9238a6b31f9401c4ecdb63f
Shinya Sasaki <shinya.sasaki@kek.jp>
2023-03-29 17:41:27 +0900

    ENH: update func param component with Segment ui

--- src/components/FunctionParam.tsx
+++ src/components/FunctionParam.tsx
@@ -1,5 +1,6 @@
-import React, { KeyboardEvent, FormEvent, FocusEvent } from 'react';
-import Autosuggest from 'react-autosuggest';
+import React from 'react';
+import { SegmentInput, Segment } from '@grafana/ui';
+import { toSelectableValue } from './toSelectableValue';
 
 export interface FunctionParamProps {
   param: string;
@@ -9,120 +10,46 @@ export interface FunctionParamProps {
   onRunQuery: () => void;
 }
 
-interface State {
-  suggestions: any[];
-  focused: boolean;
-}
-
-const getSuggestionValue = (suggestion: any) => {
-  return suggestion;
-};
-
-const renderSuggestion = (suggestion: any) => {
-  return <span>{suggestion}</span>;
-};
-
-class FunctionParam extends React.PureComponent<FunctionParamProps, State> {
-  state = { suggestions: [], focused: false };
+class FunctionParam extends React.PureComponent<FunctionParamProps> {
 
   constructor(props: FunctionParamProps) {
     super(props);
   }
 
-  onChange = (paramIndex: number, event: FormEvent<any>, { newValue }: any) => {
-    this.props.onChange(paramIndex, String(newValue));
-  };
-
-  onKeydownEnter = (event: KeyboardEvent<HTMLInputElement>) => {
-    if (event.keyCode === 13) {
-      event.currentTarget.blur();
-    }
-  };
-
-  onPVSuggestionsFetchRequested = ({ value }: { value: any }, options: string[] | undefined) => {
-    const suggestions = options === undefined ? [] : options;
-    this.setState({
-      suggestions: suggestions,
-    });
-  };
-
-  onPVSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: [],
-    });
-  };
-
-  onSuggestionsSelected = (
-    event: React.FormEvent<any>,
-    {
-      suggestion,
-      suggestionValue,
-      suggestionIndex,
-      sectionIndex,
-      method,
-    }: {
-      suggestion: any;
-      suggestionValue: string;
-      suggestionIndex: number;
-      sectionIndex: number | null;
-      method: 'click' | 'enter';
-    }
-  ) => {
-    if (method === 'enter') {
-      event.currentTarget.blur();
-    }
-  };
-
-  onFocus = (event: FocusEvent<any>) => {
-    this.setState({
-      focused: true,
-    });
-  };
-
-  onBlur = (event: FocusEvent<any>) => {
+  onChange = (paramIndex: number, value: any) => {
+    console.log("test");
     const { onRunQuery } = this.props;
-    this.setState({
-      focused: false,
-    });
+    this.props.onChange(paramIndex, String(value));
     onRunQuery();
   };
 
-  calcInputWidth = (focused: boolean, param: string, placeholder: string) => {
-    const paramLength = param.length;
-
-    if (focused) {
-      return paramLength < 13 ? `13ch` : `${paramLength + 2}ch`;
-    }
-
-    return paramLength < 1 ? `${placeholder.length}ch` : `${paramLength}ch`;
-  };
-
   render() {
-    const { suggestions, focused } = this.state;
     const { param, paramDef, index } = this.props;
-    const inputWidth = this.calcInputWidth(focused, param, paramDef.name);
-    return (
-      <div className="aa-func-param">
-        <Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={(e) => this.onPVSuggestionsFetchRequested(e, paramDef.options)}
-          onSuggestionsClearRequested={this.onPVSuggestionsClearRequested}
-          onSuggestionSelected={this.onSuggestionsSelected}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={{
-            value: param,
-            className: 'tight-form-func-param',
-            placeholder: paramDef ? paramDef.name : '',
-            spellCheck: false,
-            onChange: (e, params) => this.onChange(index, e, params),
-            onBlur: this.onBlur,
-            onKeyDown: this.onKeydownEnter,
-            onFocus: this.onFocus,
-            style: { width: inputWidth, fontFamily: 'Consolas, "Courier New", Courier, Monaco, monospace' },
+
+    if (paramDef.options === undefined) {
+      return (
+        <SegmentInput
+          value={param}
+          placeholder={paramDef.name}
+          onChange={(text) => {
+            this.onChange(index, text);
           }}
         />
-      </div>
+      );
+    }
+
+    const options = paramDef.options.map(toSelectableValue);
+
+    return (
+      <Segment
+        value={param}
+        options={options}
+        placeholder="Enter a value"
+        onChange={(item) => {
+          this.onChange(index, item.value);
+        }}
+        width={param.length}
+      />
     );
   }
 }
