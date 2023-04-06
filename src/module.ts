b8de207eedcaf24f3eba5f4da9742038d4c169a5
Shinya Sasaki <shinya.sasaki@kek.jp>
2023-04-06 16:25:24 +0900

    DEP: remove stylesheets from front end

--- src/module.ts
+++ src/module.ts
@@ -1,13 +1,7 @@
 import { DataSourcePlugin } from '@grafana/data';
-import { loadPluginCss } from '@grafana/runtime';
 import { DataSource } from './DataSource';
 import { ConfigEditor, QueryEditor } from './components';
 import { AAQuery, AADataSourceOptions } from './types';
-
-loadPluginCss({
-  dark: 'plugins/sasaki77-archiverappliance-datasource/styles/dark.css',
-  light: 'plugins/sasaki77-archiverappliance-datasource/styles/light.css',
-});
 
 export const plugin = new DataSourcePlugin<DataSource, AAQuery, AADataSourceOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
