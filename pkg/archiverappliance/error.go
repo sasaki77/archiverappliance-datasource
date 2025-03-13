2e14206ffaf198b07cca444eb1c266a8fa62f765
Shinya Sasaki <shinya.sasaki@kek.jp>
2025-03-13 10:10:25 +0900

    BUG: fix to return error when a http status code is not 200

--- pkg/archiverappliance/error.go
+++ pkg/archiverappliance/error.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	errEmptyResponse         = errors.New("response is empty")
+	errResponseStatusCode    = errors.New("response status code is invalid")
 	errIllegalPayloadType    = errors.New("response from Archiver Appliance might be illegal payload type")
 	errIllegalFieldName      = errors.New("unavailable field name")
 	errFailedToParsePBFormat = errors.New("failed to parse the PB format response")
