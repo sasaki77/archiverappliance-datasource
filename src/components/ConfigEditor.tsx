09f5968e8503e6634085d8049651a5556a985c3c
Shinya Sasaki <shinya.sasaki@kek.jp>
2025-07-29 07:30:20 +0000

    DEP: fix ConfigEditor without DataSourceHttpSettings

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -1,10 +1,19 @@
 import React, { PureComponent, ChangeEvent } from 'react';
-import { DataSourceHttpSettings, InlineSwitch, InlineField, Combobox, ComboboxOption } from '@grafana/ui';
+import { Input, Field, Label, Icon, Tooltip, Combobox, ComboboxOption, Divider, Switch, Stack } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { AADataSourceOptions, operatorList } from '../types';
 import { toComboboxOption } from './utils';
+import {
+  ConfigSection,
+  ConnectionSettings,
+  Auth,
+  AdvancedHttpSettings,
+  convertLegacyAuthProps,
+  EditorStack,
+  ConfigSubSection,
+} from '@grafana/plugin-ui';
 
-const LABEL_WIDTH = 26;
+//const LABEL_WIDTH = 26;
 const operatorOptions: Array<ComboboxOption<string>> = operatorList.map(toComboboxOption);
 
 export type Props = DataSourcePluginOptionsEditorProps<AADataSourceOptions>;
@@ -64,77 +73,142 @@ export class ConfigEditor extends PureComponent<Props> {
 
     return (
       <>
-        <DataSourceHttpSettings
-          defaultUrl="http://localhost:17668/retrieval"
-          dataSourceConfig={options}
-          onChange={onOptionsChange}
+        <ConnectionSettings config={options} onChange={onOptionsChange} />
+
+        <Divider spacing={4} />
+
+        <Auth
+          {...convertLegacyAuthProps({
+            config: options,
+            onChange: onOptionsChange,
+          })}
         />
-        <h3 className="page-heading">Misc</h3>
-        <InlineField
-          label="Use Backend"
-          labelWidth={LABEL_WIDTH}
-          tooltip="Checking this option will enable the data retrieval with backend. The archived data is retrieved and processed on Grafana server, then the data is sent to Grafana client."
-          interactive={true}
-        >
-          <InlineSwitch value={options.jsonData.useBackend ?? false} onChange={this.onUseBEChange} />
-        </InlineField>
-        <InlineField
-          label="Default Operator"
-          labelWidth={LABEL_WIDTH}
-          interactive={true}
-          tooltip={
-            <p>
-              Controls processing of data during data retrieval. Refer{' '}
-              <a
-                href="https://epicsarchiver.readthedocs.io/en/latest/user/userguide.html#processing-of-data"
-                target="_blank"
-                rel="noopener noreferrer"
+
+        <Divider spacing={4} />
+
+        <ConfigSection title="Advanced settings" isCollapsible>
+          <Stack gap={3} direction="column">
+            <AdvancedHttpSettings config={options} onChange={onOptionsChange} />
+            <ConfigSubSection title="Data Retrieval Options">
+              <Field
+                label={
+                  <Label>
+                    <EditorStack gap={0.5}>
+                      <span>Use Backend</span>
+                      <Tooltip
+                        content={
+                          <span>
+                            Checking this option will enable the data retrieval with backend. The archived data is
+                            retrieved and processed on Grafana server, then the data is sent to Grafana client.
+                          </span>
+                        }
+                      >
+                        <Icon name="info-circle" size="sm" />
+                      </Tooltip>
+                    </EditorStack>
+                  </Label>
+                }
               >
-                Archiver Appliance User Guide
-              </a>{' '}
-              about processing of data. Special operator <code>raw</code> and <code>last</code> are also available.{' '}
-              <code>raw</code> allows to retrieve the data without processing. <code>last</code> allows to retrieve the
-              last data in the specified time range.
-            </p>
-          }
-        >
-          <Combobox
-            value={options.jsonData.defaultOperator}
-            options={operatorOptions}
-            onChange={this.onOperatorChange}
-            placeholder="mean"
-          />
-        </InlineField>
-        <InlineField
-          label="Hide Invalid"
-          labelWidth={LABEL_WIDTH}
-          interactive={true}
-          tooltip={<p>Hide sample data whose severity is invalid with a null value.</p>}
-        >
-          <InlineSwitch value={options.jsonData.hideInvalid ?? false} onChange={this.onHideInvalidChange} />
-        </InlineField>
-        <InlineField
-          label="Use live feature (Alpha)"
-          labelWidth={LABEL_WIDTH}
-          interactive={true}
-          tooltip="(Caution) This is a alpha feature. Live feature provides live updating with PVWS WebSocket server."
-        >
-          <InlineSwitch value={options.jsonData.useLiveUpdate ?? false} onChange={this.onUseLiveUpdateChange} />
-        </InlineField>
-        <InlineField
-          label="PVWS URI (Alpha)"
-          labelWidth={LABEL_WIDTH}
-          interactive={true}
-          tooltip="URI for PVWS WebSocket server."
-        >
-          <input
-            type="text"
-            value={options.jsonData.liveUpdateURI}
-            className="gf-form-input max-width-15"
-            placeholder="ws://localhost:8080/pvws/pv"
-            onChange={this.onLiveUpdateURIChange}
-          />
-        </InlineField>
+                <Switch value={options.jsonData.useBackend ?? false} onChange={this.onUseBEChange} />
+              </Field>
+
+              <Field
+                label={
+                  <Label>
+                    <EditorStack gap={0.5}>
+                      <span>Default Operator</span>
+                      <Tooltip
+                        content={
+                          <p>
+                            Controls processing of data during data retrieval. Refer{' '}
+                            <a
+                              href="https://epicsarchiver.readthedocs.io/en/latest/user/userguide.html#processing-of-data"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              Archiver Appliance User Guide
+                            </a>{' '}
+                            <code>raw</code> allows to retrieve the data without processing. <code>last</code> allows to
+                            retrieve the last data in the specified time range.
+                          </p>
+                        }
+                      >
+                        <Icon name="info-circle" size="sm" />
+                      </Tooltip>
+                    </EditorStack>
+                  </Label>
+                }
+              >
+                <Combobox
+                  value={options.jsonData.defaultOperator}
+                  options={operatorOptions}
+                  width={40}
+                  onChange={this.onOperatorChange}
+                  placeholder="mean"
+                />
+              </Field>
+
+              <Field
+                label={
+                  <Label>
+                    <EditorStack gap={0.5}>
+                      <span>Hide Invalid</span>
+                      <Tooltip content={<span>Hide sample data whose severity is invalid with a null value.</span>}>
+                        <Icon name="info-circle" size="sm" />
+                      </Tooltip>
+                    </EditorStack>
+                  </Label>
+                }
+              >
+                <Switch value={options.jsonData.hideInvalid ?? false} onChange={this.onHideInvalidChange} />
+              </Field>
+            </ConfigSubSection>
+
+            <ConfigSubSection title="Live Feature Options">
+              <Field
+                label={
+                  <Label>
+                    <EditorStack gap={0.5}>
+                      <span>Use live feature (Alpha)</span>
+                      <Tooltip
+                        content={
+                          <span>
+                            (Caution) This is an alpha feature. Live feature provides live updating with PVWS WebSocket
+                            server.
+                          </span>
+                        }
+                      >
+                        <Icon name="info-circle" size="sm" />
+                      </Tooltip>
+                    </EditorStack>
+                  </Label>
+                }
+              >
+                <Switch value={options.jsonData.useLiveUpdate ?? false} onChange={this.onUseLiveUpdateChange} />
+              </Field>
+
+              <Field
+                label={
+                  <Label>
+                    <EditorStack gap={0.5}>
+                      <span>PVWS URI (Alpha)</span>
+                      <Tooltip content={<span>URI for PVWS WebSocket server.</span>}>
+                        <Icon name="info-circle" size="sm" />
+                      </Tooltip>
+                    </EditorStack>
+                  </Label>
+                }
+              >
+                <Input
+                  value={options.jsonData.liveUpdateURI}
+                  placeholder="ws://localhost:8080/pvws/pv"
+                  width={40}
+                  onChange={this.onLiveUpdateURIChange}
+                />
+              </Field>
+            </ConfigSubSection>
+          </Stack>
+        </ConfigSection>
       </>
     );
   }
